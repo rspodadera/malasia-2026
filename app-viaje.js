dabf3ef503c282e0c31b73168d500aaefd1c560a
rspodadera <rafael.s.poda@gmail.com>
2026-07-26 18:10:57 +0200

    Update app-viaje.js

--- app-viaje.js
+++ app-viaje.js
@@ -1,4 +1,4 @@
-const hoy = new Date("2026-07-27T12:00:00");
+const hoy = new Date("2026-08-27T12:00:00");
 
 const inicioViaje = new Date("2026-08-10T00:00:00");
 const finViaje = new Date("2026-08-29T23:59:59");
