dab9654f22501124f3d021052660390d30b93122
rspodadera <rafael.s.poda@gmail.com>
2026-07-26 14:54:00 +0200

    Create viaje.js

---
+++ viaje.js
@@ -0,0 +1,50 @@
+const viaje = [
+  {
+    inicio: "2026-08-10",
+    fin: "2026-08-12",
+    lugar: "Malaca",
+    icono: "🏛️",
+    hotel: "Eco Tree Hotel Melaka",
+    plan: "Jonker Street · Río de Malaca · Stadthuys"
+  },
+  {
+    inicio: "2026-08-12",
+    fin: "2026-08-14",
+    lugar: "Taman Negara",
+    icono: "🌿",
+    hotel: "Benteng Taman Negara",
+    plan: "Selva · Canopy Walk · Naturaleza"
+  },
+  {
+    inicio: "2026-08-14",
+    fin: "2026-08-16",
+    lugar: "Cameron Highlands",
+    icono: "🍃",
+    hotel: "De'La Ferns Hotel",
+    plan: "Plantaciones de té · Mercados · Senderos"
+  },
+  {
+    inicio: "2026-08-16",
+    fin: "2026-08-20",
+    lugar: "George Town",
+    icono: "🎨",
+    hotel: "Airbnb Penang",
+    plan: "Street Art · Patrimonio UNESCO · Gastronomía"
+  },
+  {
+    inicio: "2026-08-20",
+    fin: "2026-08-24",
+    lugar: "Perhentian",
+    icono: "🏝️",
+    hotel: "Barat Perhentian Resort",
+    plan: "Playa · Snorkel · Relax"
+  },
+  {
+    inicio: "2026-08-24",
+    fin: "2026-08-29",
+    lugar: "Kuala Lumpur",
+    icono: "🏙️",
+    hotel: "Eaton Residence",
+    plan: "Petronas · Compras · Ciudad"
+  }
+];
